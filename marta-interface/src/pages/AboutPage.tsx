3d8930b4b6759a0f971f465f7a58301f419b702e
Keshav Worathur <keshavworathur@gmail.com>
2025-10-23 21:17:55 -0400

    feat: add about section to website

--- marta-interface/src/pages/AboutPage.tsx
+++ marta-interface/src/pages/AboutPage.tsx
@@ -1,8 +1,14 @@
 const AboutPage = () => {
-    return (<div>
-            <h2>About</h2>
-            <hr />
-            <p>MARTA is a transit authority serving the metropolitan Atlanta area.</p>
+    return (<div className='p-4'>
+            <section className='m-16 text-left'>
+                <h2 className='text-2xl'>I. About</h2>
+                <p className='pt-2'>MARTA is a transit authority serving the metropolitan Atlanta area.</p>
+            </section>
+            <section className='m-16 text-left'>
+                <h2 className='text-2xl'>II. Customer Charter</h2>
+                <p className='pt-2'>MARTA is a transit authority serving the metropolitan Atlanta area.</p>
+            </section>
+            
         </div>);
 }
  
